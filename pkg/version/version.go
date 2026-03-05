215273abf3ff0b6134e5e1e27f8494edfda676a0
Vitaliy Ryumshyn <vr@samebits.com>
2026-03-05 12:36:25 +0100

    release: set version to 0.3.0

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -2,7 +2,7 @@ package version
 
 var (
 	// Version is the build/runtime version string for Evidra Benchmark binaries.
-	Version = "0.3.0-dev"
+	Version = "0.3.0"
 	// Commit describes the revision or commit hash used to build the binary.
 	Commit = "dev"
 	// Date stores the build timestamp.
